c4e5a2248009b60740c8e04e0d878bab88f88f3b
RYORDAN PANTER <102066512@student.swin.edu.au>
2019-10-06 17:14:35 +1100

    LinkedListTests Finished, all passing

--- Exercises/LinkedList.cs
+++ Exercises/LinkedList.cs
@@ -5,48 +5,63 @@ namespace Exercises
         public string Data;
         public ListNode Next;
 
-        public ListNode(string data, ListNode next = null) {
+
+        public ListNode(string data, ListNode next = null)
+        {
             this.Data = data;
-            this.Next = null;
+            this.Next = next;
         }
+
+        public ListNode() { }
     }
 
-    public class LinkedList {
+    public class LinkedList
+    {
         public ListNode Head;
 
-        public LinkedList() {
+        public LinkedList()
+        {
             Head = null;
         }
 
-        public void AddToEnd(string newData) {
+        public void AddToEnd(string newData)
+        {
             ListNode newNode = new ListNode(newData, null);
-            
-            if (Head == null) {
+
+            if (Head == null)
+            {
                 Head = newNode;
                 return;
-            } 
-            
+            }
+
             ListNode current = Head;
 
-            while (current.Next != null) {
+            while (current.Next != null)
+            {
                 current = current.Next;
             }
 
-                current.Next = newNode;
+            current.Next = newNode;
         }
-        
-        public ListNode GetNodeAt(int index) {
+
+        public ListNode GetNodeAt(int index)
+        {
             int count = 0;
 
-            if (index < 0) {
+            if (index < 0)
+            {
                 return null;
             }
-            
+
             ListNode current = Head;
-            while (count < index) {
-                if (current.Next != null) {
+            while (count < index)
+            {
+                if (current.Next != null)
+                {
                     current = current.Next;
-                } else {
+                }
+                else
+                {
                     return null;
                 }
                 count++;
@@ -55,11 +70,14 @@ namespace Exercises
             return current;
         }
 
-        public bool Find(string searchTerm) {
+        public bool Find(string searchTerm)
+        {
             ListNode current = Head;
 
-            while (current != null) {
-                if (current.Data == searchTerm) {
+            while (current != null)
+            {
+                if (current.Data == searchTerm)
+                {
                     return true;
                 }
 
@@ -73,8 +91,22 @@ namespace Exercises
         /// Returns the number of nodes in the Linked List
         /// </summary>
         /// <returns>int: count</returns>
-        public int Count() {
-            return -1;
+        public int Count()
+        {
+            int Count = 1;
+            ListNode Current = Head;
+            while (Current.Next != null)
+            {
+                Count++;
+                Current = Current.Next;
+            }
+            return Count;
+        }
+
+        public ListNode InsertNewListNode(string data, ListNode node)
+        {
+            ListNode next = node;
+            return new ListNode(data, next);
         }
 
         /// <summary>
@@ -82,8 +114,10 @@ namespace Exercises
         /// </summary>
         /// <param name="data"></param>
         /// <returns>success: true</returns>
-        public bool AddToStart(string data) {
-            return false;
+        public bool AddToStart(string data)
+        {
+            Head = InsertNewListNode(data, Head);
+            return true;
         }
 
         /// <summary>
@@ -93,8 +127,32 @@ namespace Exercises
         /// <param name="data"></param>
         /// <param name="index"></param>
         /// <returns></returns>
-        public bool AddNodeAt(string data, int index) {
-            return false;
+        public bool AddNodeAt(string data, int index)
+        {
+            if (index < 1) return false;
+            ListNode current = Head;
+            ListNode previous = null;
+
+            int i = 0;
+            // stepping through each node 
+            while (i < index)
+            { 
+                if (current.Next == null) break;
+                previous = current;
+                current = current.Next;
+                i++;
+            }
+            // add mew node
+            while (i < index)
+            { 
+                previous = current;
+                current = new ListNode();
+                previous.Next = current;
+                i++;
+            }
+
+            previous.Next = InsertNewListNode(data, current);
+            return true;
         }
 
         /// <summary>
@@ -102,8 +160,23 @@ namespace Exercises
         /// </summary>
         /// <param name="index"></param>
         /// <returns></returns>
-        public bool DeleteNodeAt(int index) {
-            return false;
+        public bool DeleteNodeAt(int index)
+        {
+            ListNode current = Head;
+            ListNode previous = null;
+            for (int i = 0; i < index; i++)
+            {
+                if (current == null) break;
+                previous = current;
+                current = current.Next;
+            }
+            // node doesn't exist
+            if (current == null)
+            {
+                return false;
+            }
+            previous.Next = current.Next;
+            return true;
         }
     }
 }
